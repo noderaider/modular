9e846b6f0d6c77e39b3d9b2a059dfe2c79a808e4
Cole Chamberlain <cole.chamberlain@gmail.com>
2016-10-20 12:22:00 -0700

    switched create-react-package to create-component-module

---
+++ packages/create-component-module/README.md
@@ -0,0 +1,11 @@
+# create-component-module
+
+**Unofficial counterpart to create-react-app to allow quick scaffolding of CSS module packages.**
+
+[![NPM](https://nodei.co/npm/create-component-module.png?stars=true&downloads=true)](https://nodei.co/npm/create-component-module/)
+
+## Install
+
+`npm i -S create-component-module`
+
+## In active development, review the unit tests until documentation is added.
--- packages/create-component-module/index.js
+++ packages/create-component-module/index.js
@@ -13,21 +13,21 @@
 //   /!\ DO NOT MODIFY THIS FILE /!\
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //
-// create-react-package is installed globally on people's computers. This means
+// create-component-module is installed globally on people's computers. This means
 // that it is extremely difficult to have them upgrade the version and
 // because there's only one global version installed, it is very prone to
 // breaking changes.
 //
-// The only job of create-react-package is to init the repository and then
-// forward all the commands to the local version of create-react-package.
+// The only job of create-component-module is to init the repository and then
+// forward all the commands to the local version of create-component-module.
 //
 // If you need to add a new command, please add it to the scripts/ folder.
 //
 // The only reason to modify this file is to add more warnings and
-// troubleshooting information for the `create-react-package` command.
+// troubleshooting information for the `create-component-module` command.
 //
 // Do not make breaking changes! We absolutely don't want to have to
-// tell people to update their global version of create-react-package.
+// tell people to update their global version of create-component-module.
 //
 // Also be careful with new language features.
 // This file must work on Node 0.10+.
@@ -38,4 +38,4 @@
 
 'use strict';
 
-require('create-package').default('create-react-package', require('./package.json').version);
+require('create-package').default('create-component-module', require('./package.json').version);
