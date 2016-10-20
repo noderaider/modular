f69a1436396bbaf68297b5c7848664704449de09
Cole Chamberlain <cole.chamberlain@gmail.com>
2016-10-20 08:43:21 -0700

    extracted common logic to create-package function

--- packages/bin-utils/src/lib/__tests__/index.test.js
+++ packages/bin-utils/src/lib/__tests__/index.test.js
@@ -4,16 +4,42 @@ describe('bin-utils', () => {
   it('should be a function', () => {
     expect(typeof binUtils).toBe('object')
   })
-  it('should have rimraf', () => {
-    expect(typeof binUtils.rimraf).toBe('function')
+
+  describe('')
+
+  describe('rimraf', () => {
+    it('should be function', () => {
+      expect(typeof binUtils.rimraf).toBe('function')
+    })
+    it('should have sync function', () => {
+      expect(typeof binUtils.rimraf.sync).toBe('function')
+    })
   })
-  it('should have mkdirp', () => {
-    expect(typeof binUtils.mkdirp).toBe('function')
+
+  describe('mkdirp', () => {
+    it('should be function', () => {
+      expect(typeof binUtils.mkdirp).toBe('function')
+    })
+    it('should have sync function', () => {
+      expect(typeof binUtils.mkdirp.sync).toBe('function')
+    })
   })
-  it('should have ncp', () => {
-    expect(typeof binUtils.ncp).toBe('function')
+
+  describe('ncp', () => {
+    it('should be function', () => {
+      expect(typeof binUtils.ncp).toBe('function')
+    })
+    it('should have sync function', () => {
+      expect(typeof binUtils.ncp.sync).toBe('function')
+    })
   })
-  it('should have stat', () => {
-    expect(typeof binUtils.stat).toBe('function')
+
+  describe('stat', () => {
+    it('should be function', () => {
+      expect(typeof binUtils.stat).toBe('function')
+    })
+    it('should have sync function', () => {
+      expect(typeof binUtils.stat.sync).toBe('function')
+    })
   })
 })
---
+++ packages/create-package/README.md
@@ -0,0 +1,11 @@
+# create-package
+
+**Unofficial counterpart to create-react-app to allow quick scaffolding of lerna monorepos ready for `create-react-app` usage.**
+
+[![NPM](https://nodei.co/npm/create-package.png?stars=true&downloads=true)](https://nodei.co/npm/create-package/)
+
+## Install
+
+`npm i -S create-package`
+
+## In active development, review the unit tests until documentation is added.
