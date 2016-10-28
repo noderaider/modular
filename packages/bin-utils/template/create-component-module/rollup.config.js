4fcc9ca2db368160542e6e619e00df9febab5f71
Cole Chamberlain <cole.chamberlain@gmail.com>
2016-10-28 13:50:32 -0700

    updated packages

--- packages/bin-utils/template/create-component-module/rollup.config.js
+++ packages/bin-utils/template/create-component-module/rollup.config.js
@@ -1,14 +1,13 @@
 import babel from 'rollup-plugin-babel'
-import babelrc from 'babelrc-rollup'
-import nodeResolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
+import nodeResolve from 'rollup-plugin-node-resolve'
 
 export default (
   { entry: 'src/index.js'
   , dest: 'dist/bundle.js'
-  , format: 'umd'
-  , moduleName: require('./package.json').name.split('-').map((x, i) => i === 0 ? x : `${x[0]}${x.slice(1)}`).join('')
-  , plugins:  [ babel(babelrc())
+  , format: 'cjs'
+  , external: [ 'react' ]
+  , plugins:  [ babel()
               , nodeResolve({ jsnext: true, main: true })
               , commonjs({ include: 'node_modules/**' })
               ]
---
+++ packages/bin-utils/template/create-component-module/src/__tests__/index.test.js
@@ -0,0 +1,7 @@
+import lib from '../'
+
+describe('lib', () => {
+  it('is a function', () => {
+    expect(typeof lib).toBe('function')
+  })
+})
