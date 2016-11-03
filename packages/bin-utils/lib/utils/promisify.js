aff1358bd2e88cdab50067d1b37f2dcb2633574d
Cole Chamberlain <cole.chamberlain@gmail.com>
2016-11-02 21:54:40 -0700

    $(echo v0.13.1)

--- packages/bin-utils/lib/utils/promisify.js
+++ packages/bin-utils/lib/utils/promisify.js
@@ -3,14 +3,22 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+
+var _promise = require('babel-runtime/core-js/promise');
+
+var _promise2 = _interopRequireDefault(_promise);
+
 exports.default = promisify;
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 function promisify(fn) {
   var promisified = function promisified() {
     for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
       args[_key] = arguments[_key];
     }
 
-    return new Promise(function (resolve, reject) {
+    return new _promise2.default(function (resolve, reject) {
       try {
         fn.apply(undefined, args.concat([function () {
           if ((arguments.length <= 0 ? undefined : arguments[0]) instanceof Error) reject(arguments.length <= 0 ? undefined : arguments[0]);else resolve.apply(undefined, arguments);
