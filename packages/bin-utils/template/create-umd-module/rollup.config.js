9ada05f57b14350ae57dec320cbd67a5a06cf7b0
Cole Chamberlain <cole.chamberlain@gmail.com>
2016-11-02 20:15:37 -0700

    updated rollup to use jsnext and all packages with eslintrc

--- packages/bin-utils/template/create-umd-module/rollup.config.js
+++ packages/bin-utils/template/create-umd-module/rollup.config.js
@@ -4,12 +4,13 @@ import nodeResolve from 'rollup-plugin-node-resolve'
 
 export default (
   { entry: 'src/index.js'
-  , dest: 'dist/bundle.js'
-  , format: 'umd'
+  , targets:  [ { dest: 'dist/bundle.umd.js', format: 'umd' }
+              , { dest: 'dist/bundle.es.js', format: 'es' }
+              ]
   , moduleName: require('./package.json').name.split('-').map((x, i) => i === 0 ? x : `${x[0]}${x.slice(1)}`).join('')
   , plugins:  [ babel({ babelrc: false
                       , exclude: 'node_modules/**'
-                      , presets: [ [ 'es2015', { modules: false } ], 'stage-0' ]
+                      , presets: [ [ 'latest', { modules: false } ], 'stage-2' ]
                       , plugins: [ 'transform-runtime' ]
                       , runtimeHelpers: true
                       })
